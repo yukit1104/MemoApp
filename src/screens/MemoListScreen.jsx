e76cf27dd7b30e06f331421d31bcdb81313ca3f9
yukit1104 <78680935+yukit1104@users.noreply.github.com>
2021-02-26 18:03:07 +0900

    Install @expo/vector-icons

--- src/screens/MemoListScreen.jsx
+++ src/screens/MemoListScreen.jsx
@@ -10,7 +10,7 @@ export default function App() {
     <View style={styles.container}>
       <AppBar />
       <MemoList />
-      <CircleButton>+</CircleButton>
+      <CircleButton name="plus" />
     </View>
   );
 }
